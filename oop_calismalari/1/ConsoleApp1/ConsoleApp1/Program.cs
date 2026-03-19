2bd15993f41281b24dbddf3c43f5c81ede38109d
halilbasarr <hall.basrr@gmail.com>
2026-03-19 19:43:27 +0300

    oop çalışmaları eklendi

---
+++ oop_calismalari/1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ogrenci_bilgileri ogrenci1 = new ogrenci_bilgileri(12345, "Ahmet", "Yılmaz", 80, 85, 90, "Ankara Üniversitesi");
+            ogrenci_bilgileri ogrenci2 = new ogrenci_bilgileri(54321, "Ayşe", "Demir", 75, 80, 85, "İstanbul Üniversitesi");
+            ogrenci_bilgileri ogrenci3 = new ogrenci_bilgileri(67890, "Mehmet", "Kara", 70, 75, 80, "Ege Üniversitesi");
+            ogrenci_bilgileri ogrenci4 = new ogrenci_bilgileri(98765, "Elif", "Çelik", 85, 90, 95, "Boğaziçi Üniversitesi");
+
+            Console.WriteLine("....Hoşgeldiniz, aşağıdaki seçeneklerden birini seçiniz....\n");
+            Console.WriteLine("1-Öğrenci Bilgilerini Göster");
+            Console.WriteLine("2-Öğrenci Ortalamasını Göster");
+            Console.WriteLine("3-Öğrenci Okulunu Göster");
+            Console.WriteLine("4-Çıkış");
+
+
+
+            while (true)
+            {
+                string secim = Console.ReadLine();
+                switch (secim)
+                {
+                    case "1":
+                        ogrenci1.ogrenci_bilgilerini_goster();
+                        break;
+
+                    case "2":
+                        ogrenci1.ogrenci_not_hesapla();
+                        break;
+                    
+                    case "3":
+                        ogrenci1.okul_bilgilerini_goster();
+                        break;
+
+                    case "4":
+                        Console.WriteLine("Çıkış yapılıyor...");
+                        return;
+
+                }
+            }
+
+            
+
+
+        }
+    }
+}
---
+++ oop_calismalari/1/ConsoleApp1/ConsoleApp1/ogrenci_bilgileri.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class ogrenci_bilgileri
+    {
+        private int ogrenci_no;
+        private string ogrenci_ad;
+        private string ogrenci_soyad;
+        private int vize1;
+        private int vize2;
+        private int final;
+        private string okul_ismi;
+
+        public ogrenci_bilgileri(int ogrenci_no, string ogrenci_ad, string ogrenci_soyad, int vize1, int vize2, int final, string okul_ismi)
+        {
+            this.ogrenci_no = ogrenci_no;
+            this.ogrenci_ad = ogrenci_ad;
+            this.ogrenci_soyad = ogrenci_soyad;
+            this.vize1 = vize1;
+            this.vize2 = vize2;
+            this.final = final;
+            this.okul_ismi = okul_ismi;
+        }
+
+        public void ogrenci_bilgilerini_goster()
+        {
+            Console.WriteLine("Öğrenci No: " + ogrenci_no);
+            Console.WriteLine("Ad: " + ogrenci_ad);
+            Console.WriteLine("Soyad: " + ogrenci_soyad);
+            Console.WriteLine("Vize 1: " + vize1);
+            Console.WriteLine("Vize 2: " + vize2);
+            Console.WriteLine("Final: " + final);
+            Console.WriteLine("Okul İsmi: " + okul_ismi);
+        }
+
+        public double ogrenci_not_hesapla()
+        {
+            double ortalama = (vize1 * 0.2) + (vize2 * 0.2) + (final * 0.6);
+            Console.WriteLine("Ortalama Not: " + ortalama);
+            return ortalama;
+        }
+
+        public void okul_bilgilerini_goster()
+        {
+            Console.WriteLine("Okul İsmi: " + okul_ismi);
+        }
+
+
+
+    }
+}
